71594bf3c52a9c157619b9510be0c175a3d584b8
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2024-07-02 23:58:38 +0900

    #8 vite.config.tsのportオプションを明示的に指定した。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/iidx_training_online',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: '../docs',
-  },
-})
+    base: "/iidx_training_online",
+    plugins: [vue()],
+    server: {
+        port: 5173,
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        outDir: "../docs",
+    },
+});
